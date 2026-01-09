af97e280e6761f4390206eca789a744f70bfd4ee
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2026-01-09 18:46:23 +0700

    Membuat file EditViewModel.kt , tambah UIStateSiswa dan validasi input edit

---
+++ app/src/main/java/com/example/pertemuan14/viewmodel/EditViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.pertemuan14.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan14.modeldata.DetailSiswa
+import com.example.pertemuan14.modeldata.UIStateSiswa
+import com.example.pertemuan14.modeldata.toDataSiswa
+import com.example.pertemuan14.modeldata.toUiStateSiswa
+import com.example.pertemuan14.repositori.RepositorySiswa
+import com.example.pertemuan14.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(
+                detailSiswa = detailSiswa,
+                isEntryValid = validasiInput(detailSiswa)
+            )
+    }
+
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
